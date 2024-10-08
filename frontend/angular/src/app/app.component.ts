bcd0cbc882423a364a9deeeb74ea0b7daca782b3
kareem <kareemtabdo@gmail.com>
2024-10-08 13:44:48 -0400

    Exploring Angular by building dynamic web applications, focusing on components, services, routing, and API integrations.

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MyFirstComponent} from './my-first-component/my-first-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular';
+  clickCount = 0;
+
+  handleChildClick (){
+    this.clickCount++;
+  }
 }
