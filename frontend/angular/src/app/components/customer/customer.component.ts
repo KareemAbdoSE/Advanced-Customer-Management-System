237fb6f18b896cc0e09edf7368321e3a17be7ec0
kareem <kareemtabdo@gmail.com>
2024-10-13 18:21:22 -0400

    Added Create Customer Feature
    Added an option for customers to register and improved profile picture generation to be random. Updated components and services to support these changes and adjusted environment settings for development.

--- frontend/angular/src/app/components/customer/customer.component.ts
+++ frontend/angular/src/app/components/customer/customer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerDTO } from '../../models/customer-dto';
 import { CustomerService } from '../../services/customer/customer.service';
+import { CustomerRegistrationRequest } from '../../models/customer-registration-request';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-customer',
@@ -12,8 +14,10 @@ export class CustomerComponent implements OnInit {
 
   display = false;
   customers: Array<CustomerDTO> = [];
+  customer: CustomerRegistrationRequest = {};
   constructor(
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private messageService: MessageService
   ) {}
   ngOnInit(): void {
     this.findAllCustomers();
@@ -26,5 +30,23 @@ export class CustomerComponent implements OnInit {
           console.log(data);
         }
       })
+  }
+  save(customer: CustomerRegistrationRequest) {
+    if (customer) {
+      this.customerService.registerCustomer(customer)
+        .subscribe({
+          next: () => {
+            this.findAllCustomers();
+            this.display = false;
+            this.customer = {};
+            this.messageService.add(
+              {severity:'success',
+                summary: 'Customer saved',
+                detail: `Customer ${customer.name} was successfully saved`
+              }
+            );
+          }
+        });
+    }
   }
 }
